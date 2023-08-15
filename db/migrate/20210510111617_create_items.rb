7b819667f02af12aa7b584e7d91fc77f0f60bf92
Alex <alexkimanthi92@gmail.com>
2023-08-15 16:19:59 +0300

    completed lab

--- db/migrate/20210510111617_create_items.rb
+++ db/migrate/20210510111617_create_items.rb
@@ -5,6 +5,7 @@ class CreateItems < ActiveRecord::Migration[6.1]
       t.string :description
       t.integer :price
       t.belongs_to :user, null: false, foreign_key: true
+      t.integer :user_id
 
       t.timestamps
     end
